9ac7a365a846a3e8d73dbfa9a021350afcb0a65c
うすしお <108047349+ursidsn@users.noreply.github.com>
2025-01-01 21:10:25 +0900

    あけおめ

--- hp/script/main.js
+++ hp/script/main.js
@@ -1,7 +1,20 @@
+import run from "./interpreter.js"
 const programArea = document.querySelector("#program");
 const inputArea = document.querySelector("#input");
 const outputArea = document.querySelector("#output");
+
 programArea.addEventListener("input", _ => {
     cached = false;
     statusDiv.textContent = "準備完了";
+});
+inputArea.addEventListener("input", _ => {
+});
+outputArea.addEventListener("input", _ => {
+    cached = false;
+    statusDiv.textContent = "準備完了";
+});
+
+runButton.addEventListener("click", _ => {
+    output = run(programArea.value, inputArea.value);
+    outputArea.value = output;
 });
