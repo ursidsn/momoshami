5d7b1f14227170f88f04db2a5dc2547cd7685b30
うすしお <108047349+ursidsn@users.noreply.github.com>
2025-01-11 20:44:39 +0900

    いったん最低限にしてみる

--- docs/script/main.js
+++ docs/script/main.js
@@ -5,17 +5,6 @@ const outputArea = document.querySelector("#output");
 const runButton = document.querySelector("#run");
 const stopButton = document.querySelector("#stop");
 
-programArea.addEventListener("input", _ => {
-    cached = false;
-    statusDiv.textContent = "準備完了";
-});
-inputArea.addEventListener("input", _ => {
-});
-outputArea.addEventListener("input", _ => {
-    cached = false;
-    statusDiv.textContent = "準備完了";
-});
-
 runButton.addEventListener("click", _ => {
     output = run(programArea.value, inputArea.value);
     outputArea.value = output;
