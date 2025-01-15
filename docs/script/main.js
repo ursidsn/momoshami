be801b12095adbc3251268398fa7a1461ba71c21
うすしお <108047349+ursidsn@users.noreply.github.com>
2025-01-15 11:22:07 +0900

    初歩的過ぎて泣き

--- docs/script/main.js
+++ docs/script/main.js
@@ -1,11 +1,11 @@
 import run from "./interpreter.js";
-const programArea = document.querySelector("#program");
+const codeArea = document.querySelector("#code");
 const inputArea = document.querySelector("#input");
 const outputArea = document.querySelector("#output");
 const runButton = document.querySelector("#run");
 const stopButton = document.querySelector("#stop");
 
 runButton.addEventListener("click", _ => {
-    output = run(programArea.value, inputArea.value);
+    output = run(codeArea.value, inputArea.value);
     outputArea.value = output;
 });
